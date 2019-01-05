4eae2f8a08435f6b5c5cfe2466964a67b0f2bf1c
Paweł Janik <pawel@robotyka.net.pl>
2019-01-05 14:28:15 +0100

    Project migration to PlatformIO

---
+++ Code/include/config.h
@@ -0,0 +1,11 @@
+#define controllerName "WiFi-controller-1"
+
+#define wifiSSID "SSID"
+#define wifiPasswd "PASSWORD"
+
+#define otaPasswd "12345"
+
+#define mqttIP "IP"
+#define mqttPort 1883
+#define mqttLogin "LOGIN"
+#define mqttPasswd "PASSWORD"
---
+++ Code/include/setup.h
@@ -0,0 +1,44 @@
+#include "config.h"
+
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+void WIFIsetup()
+{
+	digitalWrite(3, LOW);
+
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(wifiSSID, wifiPasswd);
+	while (WiFi.waitForConnectResult() != WL_CONNECTED)
+	{
+		delay(5000);
+		ESP.restart();
+	}
+
+
+	digitalWrite(3, HIGH);
+}
+
+void OTAsetup()
+{
+	ArduinoOTA.setHostname(controllerName);
+	ArduinoOTA.setPassword(otaPasswd);
+
+	ArduinoOTA.onStart([]()
+	{
+		String type;
+		if(ArduinoOTA.getCommand() == U_FLASH)
+		{
+			type = "sketch";
+		}
+		else
+		{
+			type = "filesystem";
+		}
+	});
+
+	ArduinoOTA.begin();
+}
